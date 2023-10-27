29a4eb72ae4acadd01a142bc845ca58483daeec9
ferzaamanda <127113405+ferzaamanda@users.noreply.github.com>
2023-10-27 14:20:46 +0700

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Hero";
+import Footer from "../components/Footer";
+
+const About = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="div">ABOUT PAGE</div>
+      <Footer />
+    </>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Hero";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="div">HOME PAGE</div>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
